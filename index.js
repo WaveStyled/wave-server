e827a3271130462664860dd78b45346667498bb5
matt(parrot) <mdaxner@ucsc.edu>
2022-04-02 10:03:45 -0700

    added ping to python when an item is added to wardrobe

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-// Imports the express library
+// Library Imports
 var express = require('express');
 var app = express();
 var cors = require("cors");
-var port = 5000;
+var request = require("request")
+
+
+// File Imports
 const DBconn = require("./connectDB");
+
+// Main Vars
+var port = 5000;
+var python_server_dest = "localhost:5001";
+const py_ping = {
+  update : '1',
+};
+
+
 
 
 // Allow ability to get data from client in json form
@@ -22,7 +34,12 @@ app.post("/add",async(req,res) => {
     var query_data = [data.R_COLOR, data.G_COLOR, data.B_COLOR, data.TYPE, data.OC_FORMAL,data.OC_SEMI_FORMAL, data.OC_CASUAL, data.OC_WORKOUT, data.OC_OUTDOORS, data.OC_COMFY, data.WE_COLD, data.WE_HOT, data.WE_RAINY, data.WE_SNOWY, data.WE_AVG_TMP];
     const add_newItem = await DBconn.query(query,query_data);
     res.json(add_newItem);
-
+    // python program should be pinged here
+    request.post({url:python_server_dest,formData: py_ping}, function optionalCallback(err,httpResponse,body){
+      if(err){
+        console.error(err.message);
+      }
+    });
   }
   catch(err) {
     console.error(err.message);
@@ -51,5 +68,5 @@ app.get("/wardrobe",async(req,res) => {
 
 // Starts the server on the port given
 app.listen(port,() => {
-  console.log("Sever has started on port: " + port);
+  console.log("Server has started on port: " + port);
 })
--- testing_files/py_to_node.py
+++ testing_files/py_to_node.py
@@ -5,3 +5,12 @@ import json
 def get_wardrobe_json():
     url = "http://localhost:5000/wardrobe"
     r = requests.get(url),json()
+
+
+import os
+from http.server import HTTPServer, CGIHTTPRequestHandler# Make sure the server is created at current directory
+os.chdir('.')
+# Create server object listening the port 80
+server_object = HTTPServer(server_address=('', 5001), RequestHandlerClass=CGIHTTPRequestHandler)
+# Start the web server
+server_object.serve_forever()
