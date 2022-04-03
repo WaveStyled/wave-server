2a7450337d8e2032f01482a759d7279c9e072183
matt-dell <mdaxner@ucsc.edu>
2022-04-02 22:01:37 -0700

    minor fixes for communcation between python server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var cors = require("cors");
 var request = require("request")
+const axios = require('axios');
 
 
 // File Imports
@@ -10,12 +11,7 @@ const DBconn = require("./connectDB");
 
 // Main Vars
 var port = 5000;
-var python_server_dest = "localhost:5001";
-const py_ping = {
-  update : '1',
-};
-
-
+var python_server_dest = "http://localhost:5001/ping";
 
 
 // Allow ability to get data from client in json form
@@ -30,17 +26,30 @@ app.use(express.json());
 app.post("/add",async(req,res) => {
   try {
     var data = req.body;
-    console.log(data.pieceid)
+   
     var query = "INSERT INTO wardrobe (PIECEID, R_COLOR,G_COLOR,B_COLOR,TYPE,OC_FORMAL,OC_SEMI_FORMAL,OC_CASUAL,OC_WORKOUT,OC_OUTDOORS,OC_COMFY, WE_COLD,WE_HOT,WE_RAINY,WE_SNOWY,WE_AVG_TMP) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16)";
     var query_data = [data.PIECEID, data.R_COLOR, data.G_COLOR, data.B_COLOR, data.TYPE, data.OC_FORMAL,data.OC_SEMI_FORMAL, data.OC_CASUAL, data.OC_WORKOUT, data.OC_OUTDOORS, data.OC_COMFY, data.WE_COLD, data.WE_HOT, data.WE_RAINY, data.WE_SNOWY, data.WE_AVG_TMP];
     const add_newItem = await DBconn.query(query,query_data);
     res.json(add_newItem);
     // python program should be pinged here
-    request.post({url:python_server_dest,formData: py_ping}, function optionalCallback(err,httpResponse,body){
-      if(err){
-        console.error(err.message);
-      }
-    });
+   //request.post({url:python_server_dest,formData: py_ping}, function optionalCallback(err,httpResponse,body){
+   //   if(err){
+   //     console.error(err.message);
+   //   }
+  //  });
+   const py_ping = {
+      "PK": data.PIECEID
+   };
+
+
+  axios.put("http://localhost:5001/ping", py_ping).then((res) =>{
+    console.log(res.data);
+  }).catch((err) => {
+    console.log(err);
+  });
+
+
+
   }
   catch(err) {
     console.error(err.message);
