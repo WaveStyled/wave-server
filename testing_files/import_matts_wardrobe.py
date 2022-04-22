85f9103e9d95f1f50161cfef460adcde93d6ce30
matt-dell <mdaxner@ucsc.edu>
2022-04-22 07:37:00 -0700

    Fixed up import_matts_wardrobe.py to make it current with Node server and Pandas DF

--- testing_files/import_matts_wardrobe.py
+++ testing_files/import_matts_wardrobe.py
@@ -8,28 +8,27 @@ today = date.today()
 # mm/dd/y
 d3 = today.strftime("%m/%d/%y")
 
-data = pd.read_csv('good_matts_wardrobe.csv') 
-url = "http://localhost:5000/add"
+data = pd.read_csv('../../wave-recommender/good_matts_wardrobe.csv') 
+url = "http://localhost:5000/add/123"
 
 for row in data.itertuples(index=False):
     myobj = {'COLOR': row.color,
 			 'TYPE': row.type,
 			 'PIECEID': row.pieceid,
-			 'OC_SEMI_FORMAL': row.semi_formal,
-			 'OC_FORMAL': row.formal,
-			 'OC_CASUAL': row.casual,
-			 'OC_WORKOUT': row.workout,
-			 'OC_OUTDOORS': row.outdoors,
-			 'OC_COMFY': row.comfy,
-			 'WE_HOT': row.hot,
-			 'WE_COLD': row.cold,
-			 'WE_RAINY': row.rainy,
-			 'WE_SNOWY': row.snowy,
-			 'WE_TYPICAL': row.typical,
+			 'OC_SEMI_FORMAL': row.oc_semi_formal,
+			 'OC_FORMAL': row.oc_formal,
+			 'OC_CASUAL': row.oc_casual,
+			 'OC_WORKOUT': row.oc_workout,
+			 'OC_OUTDOORS': row.oc_outdoors,
+			 'OC_COMFY': row.oc_comfy,
+			 'WE_HOT': row.we_hot,
+			 'WE_COLD': row.we_cold,
+			 'WE_RAINY': row.we_rainy,
+			 'WE_SNOWY': row.we_snowy,
+			 'WE_TYPICAL': row.we_typical,
 			 'RECENT_DATE_WORN' : d3,
 			 'TIMES_WORN': 0,
 			 'RATING':     0.5,
 			 'DIRTY': 0  
-							}
+			}
     r = requests.post(url,json=myobj)
-
